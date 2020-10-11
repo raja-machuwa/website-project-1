3f738a4bc225cdb16c9688fb7eaa7cfc6ef3bfe8
raja-machuwa <rkrajakumar.619@gmail.com>
2020-10-11 18:47:24 +0530

    first project

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var Slide = document.getElementById('Slide');
+var BackBtn = document.getElementById('BackBtn');
+var NextBtn = document.getElementById('NextBtn');
+
+var slideimages = new Array(
+  './images/pic-1.jpg',
+  './images/pic-2.jpg',
+  './images/pic-3.jpg',
+  './images/pic-4.jpg'
+);
+
+let i = 0;
+
+NextBtn.onclick = function () {
+  if (i < 3) {
+    Slide.src = slideimages[i + 1];
+    i++;
+  }
+};
+
+BackBtn.onclick = function () {
+  if (i > 0) {
+    Slide.src = slideimages[i - 1];
+    i--;
+  }
+};
